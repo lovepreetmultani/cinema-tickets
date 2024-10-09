28ea895d472b2b950a839ccc5032454db0eecfbe
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-10-09 01:18:12 +0100

    removing redundant code

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/repository/TicketRepository.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/repository/TicketRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.dwp.uc.pairtest.model.Ticket;
 
-import java.util.Optional;
-
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
